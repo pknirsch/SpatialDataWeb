aec0b544ffc859fd67a2dd36e16484fac275e00c
Philip Knirsch <41683047+pknirsch@users.noreply.github.com>
2024-04-25 15:31:58 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -5,3 +5,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: 'OpenStreetMap contributors',
 })
 osm.addTo(map)
+
+
+// add geoJSON polygons layer*
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data)
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
