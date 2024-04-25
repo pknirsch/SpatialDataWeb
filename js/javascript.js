0ae8ac550c9f51224c82bb1e05b0ab37b1cd24cd
Philip Knirsch <41683047+pknirsch@users.noreply.github.com>
2024-04-25 15:34:06 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -15,3 +15,98 @@ async function addDistrictsGeoJson(url) {
  polygons.addTo(map)
 }
 addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+// add popup to each feature
+function popUPinfo(feature, layer) {
+ layer.bindPopup(feature.properties.NIMI)
+}
+// add geoJSON polygons layer
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data, {
+ onEachFeature: popUPinfo,
+ })
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+
+
+// get color from feature property
+function getColor(property) {
+ switch (property) {
+ case 1:
+ return '#ff0000'
+ case 13:
+ return '#009933'
+ case 6:
+ return '#0000ff'
+ case 7:
+ return '#ff0066'
+ default:
+ return '#ffffff'
+ }
+}
+
+
+// polygon style
+function polygonStyle(feature) {
+ return {
+ fillColor: getColor(feature.properties.OBJECTID),
+ fillOpacity: 0.5,
+ weight: 1,
+ opacity: 1,
+ color: 'grey',
+ }
+}
+
+
+async function addDistrictsGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const polygons = L.geoJson(data, {
+ onEachFeature: popUPinfo,
+ style: polygonStyle,
+ })
+ polygons.addTo(map)
+}
+addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+
+
+
+// add geoJSON points layer*
+async function addCelltowersGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const markers = L.geoJson(data)
+ markers.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+
+function createCircle(feature, latlng) {
+ let options = {
+ radius: 5,
+ fillColor: 'red',
+ fillOpacity: 0.5,
+ color: 'red',
+ weight: 1,
+ opacity: 1,
+ }
+ return L.circleMarker(latlng, options)
+}
+async function addCelltowersGeoJson(url) {
+ const response = await fetch(url)
+ const data = await response.json()
+ const circles = L.geoJson(data, {
+ pointToLayer: createCircle,
+ })
+ circles.addTo(map)
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson')
+
+
+
+
+                    
