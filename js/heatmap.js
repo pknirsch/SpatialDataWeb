0cfb924331e972967c2b7dc124f06a4762556134
Philip Knirsch <41683047+pknirsch@users.noreply.github.com>
2024-04-25 17:15:05 +0300

    Update heatmap.js

--- js/heatmap.js
+++ js/heatmap.js
@@ -9,3 +9,19 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 osm.addTo(map);
 
 
+// Asynchronously load and add GeoJSON layer for cell towers, using clustering
+async function addCelltowersGeoJson(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  const markers = L.geoJson(data);
+  clusters.addLayer(markers);
+}
+addCelltowersGeoJson('geojson/tartu_city_celltowers_edu.geojson');
+
+
+
+
+// Function to reset the map to default settings
+function defaultMapSettings() {
+  map.setView([58.373523, 26.716045], 12);
+}
