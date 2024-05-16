5238db89f415a1868b708af6b1c1d42c25e58a6d
Philip Knirsch <41683047+pknirsch@users.noreply.github.com>
2024-05-16 20:10:00 +0300

    Update chloroplethmap.js

--- js/chloroplethmap.js
+++ js/chloroplethmap.js
@@ -14,7 +14,7 @@ async function addGeoJson(url) {
 const response = await fetch(url)
 const data = await response.json()
 L.choropleth(data, {
-valueProperty: 'OBJECTID',
+valueProperty: 'TOWERS',
 scale: ['#ffffff', '#ff9900'],
 steps: 5,
 mode: 'q', // q for quantile, e for equidistant
