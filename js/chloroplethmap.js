47b49f402326aa9eb08c8ab9cb10eee95e5f121c
Philip Knirsch <41683047+pknirsch@users.noreply.github.com>
2024-05-16 19:47:45 +0300

    Update chloroplethmap.js

--- js/chloroplethmap.js
+++ js/chloroplethmap.js
@@ -1,3 +1,13 @@
+// Initialize the map
+const map = L.map('map').setView([58.373523, 26.716045], 12);
+
+// Define the OpenStreetMap tile layer
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: 'OpenStreetMap contributors'
+});
+osm.addTo(map);
+
 addGeoJson('geojson/tartu_city_districts_edu.geojson')
 // add geoJSON layer
 async function addGeoJson(url) {
