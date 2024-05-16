d72e2c899904662553a776fe67d41217489683eb
Philip Knirsch <41683047+pknirsch@users.noreply.github.com>
2024-05-16 20:43:07 +0300

    Update chloroplethmap.js

--- js/chloroplethmap.js
+++ js/chloroplethmap.js
@@ -8,25 +8,28 @@ const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 osm.addTo(map);
 
-addGeoJson('geojson/tartu_city_districts_edu.geojson')
-// add geoJSON layer
+// Add geoJSON layer
 async function addGeoJson(url) {
-const response = await fetch(url)
-const data = await response.json()
-L.choropleth(data, {
-valueProperty: 'TOWERS',
-scale: ['#ffffff', '#ff9900'],
-steps: 5,
-mode: 'q', // q for quantile, e for equidistant
-style: {
-color: '#fff',
-weight: 2,
-fillOpacity: 0.8,
-},
- onEachFeature: function (feature, layer) {
+  const response = await fetch(url);
+  const data = await response.json();
+
+  L.choropleth(data, {
+    valueProperty: 'TOWERS', // Feature attribute with quantity of cell towers
+    scale: ['#f7fcf0', '#00441b'], // Colors of the scale (CSS color picker used here)
+    steps: 5, // Quantity of ranges
+    mode: 'q', // q for quantile, e for equidistant
+    style: {
+      color: '#fff',
+      weight: 2,
+      fillOpacity: 0.8
+    },
+    onEachFeature: function (feature, layer) {
       const districtName = feature.properties.NIMI; // Access the district name
       const towerCount = feature.properties.TOWERS; // Access the number of towers
       layer.bindPopup(`District: ${districtName}<br>Towers: ${towerCount}`);
-},
-}).addTo(map)
+    },
+  }).addTo(map);
 }
+
+// Call the function to add GeoJSON data to the map
+addGeoJson('geojson/tartu_city_districts_edu.geojson');
